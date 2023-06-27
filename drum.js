1db1eb07f1f523af0510effb52b0e5a0b45546a6
Umar <umar.sweden@gmail.com>
2023-06-27 15:27:54 +0200

    Hi

---
+++ drum.js
@@ -0,0 +1,52 @@
+var buttons=document.querySelectorAll("button");
+
+console.log;
+buttons.forEach(button =>{
+    button.addEventListener("click",drumfun)
+})
+
+function drumfun(){
+    var buttText=this.innerHTML;
+    makesound(buttText);
+    }
+
+
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+
+})
+
+function makesound(key){
+    switch (key) {
+        case 'w':
+            var tom1= new Audio("drumsounds/tom-1.mp3")
+            tom1.play();
+            break;
+        case 'a':
+            var tom2= new Audio("drumsounds/tom-2.mp3")
+            tom2.play();
+            break;
+        case 's':
+            var tom3= new Audio("drumsounds/tom-3.mp3")
+            tom3.play();
+            break;
+        case 'd':
+            var tom4= new Audio("drumsounds/tom-4.mp3")
+            tom4.play();
+            break;
+        case 'j':
+            var crash= new Audio("drumsounds/snare.mp3")
+            crash.play();
+            break;
+        case 'k':
+            var snare= new Audio("drumsounds/crash.mp3")
+            snare.play();
+            break;
+        case 'l':
+            var kick= new Audio("drumsounds/kick-bass.mp3")
+            kick.play();
+            break;
+}
+}
+
+
---
+++ index.js
@@ -0,0 +1,29 @@
+var buttons=document.querySelectorAll("button");
+var box=document.querySelector(".box");
+
+
+
+buttons.forEach(button => {
+    button.addEventListener("click",Calculate);
+})
+
+ function Calculate(){
+ var buttontext = this.innerText;
+ if(buttontext==="AC"){
+    box.value="";
+    return;
+ }
+ if(buttontext==="C"){
+    box.value=box.value.substr(0,box.value.length-1);
+    return;
+ }
+ if(buttontext==="="){
+    var result=box.value=eval(box.value);
+    return(result);
+}
+ else{
+    box.value += buttontext;
+    return;
+ }
+}
+ 
